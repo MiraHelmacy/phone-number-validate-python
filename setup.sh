cd716d3067c05917068b5f51c8a016b561e5c3f0
Alex Helmacy <mirahelmacy@gmail.com>
2025-02-02 11:21:59 -0500

    added setup script for linux

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#local user directory for when the files are stored.
+localDirectory="$HOME/.local/phone-number-validate"
+
+#location where the link between the path and the local directory is
+linkLocation="/usr/local/bin/phone-number-validate"
+
+#make link in path
+sudo ln -s "$localDirectory/phone-number-validate" $linkLocation
+
+# make a directory for the files to go into
+rm -rf $localDirectory && mkdir -p $localDirectory || mkdir -p $localDirectory
+
+#add execute to phone-number-validate
+chmod +x phone-number-validate
+
+#copy files to the directory
+cp README.md $localDirectory
+cp LICENSE $localDirectory
+cp phone-number-validate $localDirectory
+cp -r validate $localDirectory
+cp -r fileio $localDirectory
+
